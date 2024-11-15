6017da2a5765e55b88b556d5c11d4bc1201698aa
MuhammadJareer24 <m.ahmar2496@gmail.com>
2024-11-16 01:41:12 +0500

    completed all the required things

--- server.js
+++ server.js
@@ -1,25 +1,30 @@
-import express from "express"
-import fs from "fs"
+import express, { json } from "express"
+import fs, { stat } from "fs"
+import users from "./users.json" assert { type: "json" }
+import products from "./products.json" assert { type: "json" }
 
 const app = express()
 
 app.set("view engine", "ejs")
 
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
 let id = 0
 
-//- USER--
+//-Web Page Routes - USER
 
+// Show home page
 app.get("/", (req, res) => {
   res.render("index")
 })
 
+// Show registration form
 app.get("/register", (req, res) => {
   res.render("register")
 })
 
+// Save registration details and redirect to dashboard
 app.post("/users", (req, res) => {
   const { fullName, email, phone, password } = req.body
 
@@ -42,6 +47,7 @@ app.post("/users", (req, res) => {
   })
 })
 
+// Show Dashboard with saved user details
 app.get("/dashboard", (req, res) => {
   fs.readFile("users.json", "utf-8", (err, data) => {
     const users = JSON.parse(data)
@@ -50,9 +56,11 @@ app.get("/dashboard", (req, res) => {
   })
 })
 
+// Show profile page with the details of a particular user
 app.get("/users/:id", (req, res) => {
   fs.readFile("users.json", "utf-8", (err, data) => {
     const users = JSON.parse(data)
+
     const id = Number(req.params.id)
     const user = users.find((user) => user.id === id)
 
@@ -60,31 +68,29 @@ app.get("/users/:id", (req, res) => {
   })
 })
 
+// Change the details of a user and save the updated info. Then redirect to Dashboard
 app.patch("/users/:id", (req, res) => {
   const { fullName, email, phone, password } = req.body
 
   fs.readFile("users.json", "utf-8", (err, data) => {
     const users = JSON.parse(data)
     const id = Number(req.params.id)
-    const user = users.find((user) => user.id === id)
 
-    const updatedUsers = users.map((user) => {
-      if (user.id === id) {
-        return {
-          ...user,
-          fullName,
-          email,
-          phone,
-          password,
-        }
-      }
-      return user
-    })
+    const userIndex = users.findIndex((user) => user.id === id)
 
-    fs.writeFile("users.json", JSON.stringify(updatedUsers), (err) => {
+    users[userIndex] = {
+      ...users[userIndex],
+      fullName: fullName || users[userIndex].fullName,
+      email: email || users[userIndex].email,
+      phone: phone || users[userIndex].phone,
+      password: password || users[userIndex].password,
+    }
+
+    fs.writeFile("users.json", JSON.stringify(users), () => {
       if (err) {
         return res.status(500).send("Error updating user data.")
       }
+
       res.redirect("/dashboard")
     })
   })
@@ -104,18 +110,94 @@ app.delete("/users/:id", (req, res) => {
   })
 })
 
-//- USER API--
+//- API Routes - USER
 
-//- PRODUCTS--
+app.get("/api/users", (req, res) => {
+  return res.json(users)
+})
 
-// app.get("/", (req, res) => {
-//   res.render("index")
-// })
+app.get("/api/users/:id", (req, res) => {
+  const id = Number(req.params.id)
+  const user = users.find((user) => user.id === id)
+  res.json(user)
+})
 
+app.post("/api/users", (req, res) => {
+  const { fullName, email, phone, password } = req.body
+
+  const user = {
+    id: users.length + 1,
+    fullName,
+    email,
+    phone,
+    password,
+  }
+
+  users.push(user)
+
+  fs.writeFile("users.json", JSON.stringify(users), (err, data) => {
+    if (err) {
+      return res
+        .status(500)
+        .json({ status: "Error", message: "Failed to write user data." })
+    }
+    return res.json({ status: "Success", user })
+  })
+})
+
+app.patch("/api/users/:id", (req, res) => {
+  const { fullName, email, phone, password } = req.body
+
+  const id = Number(req.params.id)
+
+  const userIndex = users.findIndex((user) => user.id === id)
+
+  users[userIndex] = {
+    ...users[userIndex],
+    fullName: fullName || users[userIndex].fullName,
+    email: email || users[userIndex].email,
+    phone: phone || users[userIndex].phone,
+    password: password || users[userIndex].password,
+  }
+
+  fs.writeFile("users.json", JSON.stringify(users), (err, data) => {
+    return res.json({ status: "Updated", user: users[userIndex] })
+  })
+})
+
+app.delete("/api/users/:id", (req, res) => {
+  const id = Number(req.params.id)
+  const userIndex = users.findIndex((user) => user.id === id)
+
+  const deletedUser = users[userIndex]
+
+  users.splice(userIndex, 1)
+
+  fs.writeFile("users.json", JSON.stringify(users), (err) => {
+    return res.json({
+      status: "Deleted",
+      deletedUser,
+    })
+  })
+})
+
+app.listen(8000, () => console.log("Server started at PORT: 8000"))
+
+//- *******************************************************************
+
+//-Web Page Routes - PRODUCT
+
+// Show home page
+app.get("/", (req, res) => {
+  res.render("index")
+})
+
+// Show Product registration form
 app.get("/registerProduct", (req, res) => {
   res.render("registerProduct")
 })
 
+// Save registration details and redirect to Product Dashboard
 app.post("/products", (req, res) => {
   const { productName, description, price } = req.body
 
@@ -137,6 +219,7 @@ app.post("/products", (req, res) => {
   })
 })
 
+// Show Product Dashboard with saved product details
 app.get("/productDashboard", (req, res) => {
   fs.readFile("products.json", "utf-8", (err, data) => {
     const products = JSON.parse(data)
@@ -145,44 +228,40 @@ app.get("/productDashboard", (req, res) => {
   })
 })
 
+// Show Product Profile page with the details of a particular product
 app.get("/products/:id", (req, res) => {
   fs.readFile("products.json", "utf-8", (err, data) => {
     const products = JSON.parse(data)
+
     const id = Number(req.params.id)
     const product = products.find((product) => product.id === id)
-
-    if (!product) {
-      return res.status(404).send("Product not found")
-    }
 
     res.render("productProfile", { product })
   })
 })
 
+// Change the details of a product and save the updated info. Then redirect to Product Dashboard
 app.patch("/products/:id", (req, res) => {
   const { productName, description, price } = req.body
 
   fs.readFile("products.json", "utf-8", (err, data) => {
     const products = JSON.parse(data)
     const id = Number(req.params.id)
-    const product = products.find((product) => product.id === id)
 
-    const updatedProducts = products.map((product) => {
-      if (product.id === id) {
-        return {
-          ...product,
-          productName,
-          description,
-          price,
-        }
-      }
-      return product
-    })
+    const productIndex = products.findIndex((product) => product.id === id)
 
-    fs.writeFile("products.json", JSON.stringify(updatedProducts), (err) => {
+    products[productIndex] = {
+      ...products[productIndex],
+      productName: productName || products[productIndex].productName,
+      description: description || products[productIndex].description,
+      price: price || products[productIndex].price,
+    }
+
+    fs.writeFile("products.json", JSON.stringify(products), () => {
       if (err) {
         return res.status(500).send("Error updating product data.")
       }
+
       res.redirect("/productDashboard")
     })
   })
@@ -202,4 +281,71 @@ app.delete("/products/:id", (req, res) => {
   })
 })
 
-app.listen(8000, () => console.log("Server started on port 8000"))
+//- API Routes - PRODUCT
+
+app.get("/api/products", (req, res) => {
+  return res.json(products)
+})
+
+app.get("/api/products/:id", (req, res) => {
+  const id = Number(req.params.id)
+  const product = products.find((product) => product.id === id)
+  res.json(product)
+})
+
+app.post("/api/products", (req, res) => {
+  const { productName, description, price } = req.body
+
+  const product = {
+    id: products.length + 1,
+    productName,
+    description,
+    price,
+  }
+
+  products.push(product)
+
+  fs.writeFile("products.json", JSON.stringify(products), (err, data) => {
+    if (err) {
+      return res
+        .status(500)
+        .json({ status: "Error", message: "Failed to write product data." })
+    }
+    return res.json({ status: "Success", product })
+  })
+})
+
+app.patch("/api/products/:id", (req, res) => {
+  const { productName, description, price } = req.body
+
+  const id = Number(req.params.id)
+
+  const productIndex = products.findIndex((product) => product.id === id)
+
+  products[productIndex] = {
+    ...products[productIndex],
+    productName: productName || products[productIndex].productName,
+    description: description || products[productIndex].description,
+    price: price || products[productIndex].price,
+  }
+
+  fs.writeFile("products.json", JSON.stringify(products), (err, data) => {
+    return res.json({ status: "Updated", product: products[productIndex] })
+  })
+})
+
+app.delete("/api/products/:id", (req, res) => {
+  const id = Number(req.params.id)
+  const productIndex = products.findIndex((product) => product.id === id)
+
+  const deletedProduct = products[productIndex]
+
+  products.splice(productIndex, 1)
+
+  fs.writeFile("products.json", JSON.stringify(products), (err) => {
+    return res.json({
+      status: "Deleted",
+      deletedProduct,
+    })
+  })
+})
